87e27ccded74c8f53a59859c34b1e9bbb7971315
Afrin Peshimam <68803028+AfrinPeshimam@users.noreply.github.com>
2022-02-07 19:13:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="wafer-fault-detection",
     version="0.0.3",
     description="ML project",
-    author="Avnish yadav", 
+    author="Afrin Peshimam", 
     packages=find_packages(),
     license="MIT"
 )
